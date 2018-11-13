d21269876a8ff93fd5b0338d2213c3a3ea7c4a6e
regnex <sunumacbright@gmail.com>
2018-11-13 13:40:12 +0000

    get data from main.dart page

--- lib/detailspage.dart
+++ lib/detailspage.dart
@@ -1,13 +1,48 @@
+import 'package:example_app/model/movie.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatefulWidget {
-  @override
-  _DetailsPageState createState() => _DetailsPageState();
-}
+  Movie movie;
 
-class _DetailsPageState extends State<DetailsPage> {
+
+  DetailsPage(this.movie);
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  DetailsPageState createState() {
+    return new DetailsPageState();
   }
 }
+
+class DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("${widget.movie.title}"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage(
+                      "https://image.tmdb.org/t/p/w500" +
+                          widget.movie.backdrop_path),
+                    fit: BoxFit.cover,
+                  )
+              ),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(widget.movie.title)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
