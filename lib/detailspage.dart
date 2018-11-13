d18ada325bbe99bf76a85b94b9171fe89477a6f1
regnex <sunumacbright@gmail.com>
2018-11-13 11:09:08 +0000

    added details page

---
+++ lib/detailspage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
